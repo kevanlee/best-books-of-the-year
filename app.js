cc492ea6b8189b0c2119951e11e7c3d051178650
Kevan Lee <kevanlee@users.noreply.github.com>
2026-08-03 16:53:10 -0600

    Guard against invalid editorial timestamps

--- app.js
+++ app.js
@@ -2433,7 +2433,11 @@
   }
 
   function formatDate(value) {
-    return new Intl.DateTimeFormat("en-US", { month: "short", day: "numeric", year: "numeric" }).format(new Date(value));
+    const date = new Date(value);
+    if (Number.isNaN(date.getTime())) {
+      return "Recently updated";
+    }
+    return new Intl.DateTimeFormat("en-US", { month: "short", day: "numeric", year: "numeric" }).format(date);
   }
 
   function formatNumber(value) {
